f57554b1947eeb8ec62a361285c06825a35e7ad3
miura55 <acordion.piano@gmail.com>
2023-02-04 00:15:05 +0900

    Initial commit

---
+++ test/cdk-handson.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkHandson from '../lib/cdk-handson-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-handson-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkHandson.CdkHandsonStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
