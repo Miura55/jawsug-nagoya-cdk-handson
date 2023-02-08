bcd3c22d57e0d97798353b1e08f137de5609a888
miura55 <acordion.piano@gmail.com>
2023-02-08 16:31:39 +0000

    Add vite config

---
+++ todo-app/vite.config.ts
@@ -0,0 +1,13 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [react()],
+  build: {
+    outDir: "build",
+  },
+})
