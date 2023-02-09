60a8e9a0250934651f48c9e4c5b31d88c6c4ed2b
miura55 <acordion.piano@gmail.com>
2023-02-09 13:49:04 +0000

    Add empty rendering

--- todo-app/src/components/TodoList/index.tsx
+++ todo-app/src/components/TodoList/index.tsx
@@ -5,16 +5,18 @@ import { TodoListProps } from '../../types';
 
 
 export const TodoList = ({ todos, onClick }: TodoListProps) => {
+  const todoItems = todos.map((todo) => (
+    <ListItem key={todo.id}>
+      <ListItemText primary={todo.message} />
+      <Fab size='small' color='error' onClick={() => onClick(todo.id)}>
+        <Delete />
+      </Fab>
+    </ListItem>
+  ));
+
   return (
     <List sx={{ width: '100%', maxWidth: 360, marginInline: "auto" }}>
-      {todos.map((todo) => (
-        <ListItem key={todo.id}>
-          <ListItemText primary={todo.message} />
-          <Fab size='small' color='error' onClick={() => onClick(todo.id)}>
-            <Delete />
-          </Fab>
-        </ListItem>
-      ))}
+      {todoItems.length ? todoItems : <p>No messages</p>}
     </List>
   );
 };
