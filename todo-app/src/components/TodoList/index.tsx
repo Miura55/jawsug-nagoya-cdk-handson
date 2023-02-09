6e93289e774341bdfa4e8d24ed8bcd31e6dd1ea7
Miura55 <acordion.piano@gmail.com>
2023-02-09 04:47:57 +0000

    format todoList

--- todo-app/src/components/TodoList/index.tsx
+++ todo-app/src/components/TodoList/index.tsx
@@ -6,23 +6,15 @@ import { TodoListProps } from '../../types';
 
 export const TodoList = ({ todos, onClick }: TodoListProps) => {
   return (
-      // <ul>
-      //   {todos.map((todo) => (
-      //     <li key={ todo.id }>
-      //       <span>{todo.message}</span>
-      //       <button onClick={ () => onClick(todo.id) }>Delete</button>
-      //     </li>
-      //   ))}
-      // </ul>
-      <List sx={{ width: '100%', maxWidth: 360, marginInline: "auto" }}>
-        {todos.map((todo) => (
-          <ListItem key={ todo.id }>
-            <ListItemText primary={ todo.message } />
-            <Fab size='small' color='error' onClick={ () => onClick(todo.id) }>
-              <Delete />
-            </Fab>
-          </ListItem>
-        ))}
-      </List>
+    <List sx={{ width: '100%', maxWidth: 360, marginInline: "auto" }}>
+      {todos.map((todo) => (
+        <ListItem key={todo.id}>
+          <ListItemText primary={todo.message} />
+          <Fab size='small' color='error' onClick={() => onClick(todo.id)}>
+            <Delete />
+          </Fab>
+        </ListItem>
+      ))}
+    </List>
   );
 };
