168f88bdf577c8d0d7769f46b3da027d3551dcf9
Miura55 <acordion.piano@gmail.com>
2023-02-23 09:58:29 +0000

    Update Frontend system

--- todo-app/vite.config.ts
+++ todo-app/vite.config.ts
@@ -1,6 +1,6 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   server: {
